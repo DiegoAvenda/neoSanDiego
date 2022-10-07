a7b21cb04eb948b3b91e1eef426752e5469abe8c
DiegoAvenda <digago7@gmail.com>
2022-10-06 22:01:44 -0500

    path correction

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,5 @@
 <script>
-	import Header from 'C:/Users/E/Documents/cyberpunk/src/lib/components/Header.svelte';
+	import Header from '$lib/components/Header.svelte';
 	import Footer from '$lib/components/Footer.svelte';
 	import '../app.css';
 </script>
